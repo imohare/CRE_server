1c11a2a8ac2b63d2c19bdfffb7947b8c1152ec43
aoniatraxler <traxler.aonia@gmail.com>
2022-03-18 18:02:52 +0000

    artist model

--- server/model/model.artist.ts
+++ server/model/model.artist.ts
@@ -1,7 +1,16 @@
 const { Sequelize, Model, DataTypes } = require('sequelize');
 const sequelize = new Sequelize('sqlite::memory:');
 
-class Artist extends Model { }
+class Artist extends Model<Artist>{ //extends Model<Artist>
+    declare eth_address: string;
+    declare website: string;
+    declare instagram: string;
+    declare twitter: string;
+    declare discord: string;
+    declare spotify: string;
+    declare username: string;
+
+}
 Artist.init({
     eth_address: DataTypes.STRING,
     website: DataTypes.STRING,
