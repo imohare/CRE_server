e0865ddf5062321d4332356ba5d5810e2cd49c69
Imogen Hare <imogenbhare@gmail.com>
2022-03-21 12:48:13 +0000

    controllers.album getArtistAlbum working

--- server/controllers/album.ts
+++ server/controllers/album.ts
@@ -84,6 +84,35 @@ async function getArtistAlbums(req: Request, res: Response) {
   }
  }
 
-async function getArtistAlbum(req: Request, res: Response) { }
+async function getArtistAlbum(req: Request, res: Response) {
+  try {
+    if (!req.params.artistId || !req.params.albumId) {
+      res.status(400);
+      res.json('incorrect schema for request');
+    } else {
+      const artistId = req.params.artistId;
+      const albumId = req.params.albumId;
+      const artist = await Artist.findByPk(artistId);
+      const album = await Album.findByPk(albumId);
+
+      if (!artist) {
+        res.status(400);
+        res.json('Artist not found');
+      } else if (!album) {
+        res.status(400);
+        res.json('Album not found');
+      }
+      else {
+        const _albums = await Album.findAll({where: {id: albumId, ArtistId: artistId}});
+        res.json(_albums);
+        res.status(201);
+      }
+    }
+  } catch (error) {
+    console.log('error');
+    res.status(500);
+    res.json(error);
+  }
+ }
 
 module.exports = { createAlbum, getAlbums, getAlbum, getArtistAlbums, getArtistAlbum };
--- server/router.ts
+++ server/router.ts
@@ -2,26 +2,26 @@ import express from 'express';
 const router = express.Router();
 
 const { createArtist, getArtist, getArtists } = require('./controllers/artist.ts');
-const { createAlbum, getAlbum, getAlbums, getArtistAlbums } = require('./controllers/album.ts');
+const { createAlbum, getAlbum, getAlbums, getArtistAlbums, getArtistAlbum } = require('./controllers/album.ts');
 const { createEvent, getEvent, getEvents, getArtistEvents } = require('./controllers/event.ts')
 const { createMerchandise, getMerchandises, getMerchandise, getArtistMerchandises } = require('./controllers/merchandise.ts');
 
-//Artist
+// Artist
 router.post('/createArtist', createArtist);
 router.get('/getArtists', getArtists);
 router.get('/getArtist/:artistId', getArtist);
-//Album
+// Album
 router.post('/createAlbum/:artistId', createAlbum);
 router.get('/getAlbums', getAlbums);
 router.get('/getAlbum/:albumId', getAlbum);
 router.get('/getArtistAlbums/:artistId', getArtistAlbums);
-//Merchandise
+router.get('/getArtistAlbum/:albumId/:artistId', getArtistAlbum);
+// Merchandise
 router.post('/createMerchandise/:artistId', createMerchandise);
 router.get('/getMerchandises', getMerchandises);
 router.get('/getMerchandise/:merchandiseId', getMerchandise);
 router.get('/getArtistMerchandises/:artistId', getArtistMerchandises);
-
-//Event
+// Event
 router.post('/createEvent/:artistId', createEvent);
 router.get('/getEvents', getEvents);
 router.get('/getEvent/:eventId', getEvent);
