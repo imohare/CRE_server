0e926b810cd00fbbaa13ef3a74957bf8c8a239ed
Imogen Hare <imogenbhare@gmail.com>
2022-03-21 11:35:02 +0000

    controllers.merchandise.getMerchandises works

--- server/router.ts
+++ server/router.ts
@@ -1,13 +1,14 @@
 import express from 'express';
+import { getMerchandise } from './controllers/merchandise';
 const router = express.Router();
 const { createArtist } = require('./controllers/artist.ts');
 const { createAlbum } = require('./controllers/album.ts');
-const { createMerchandise } = require('./controllers/merchandise.ts');
+const { createMerchandise, getMerchandises } = require('./controllers/merchandise.ts');
 
 
 
 router.post('/createArtist', createArtist);
 router.post('/createAlbum/:artistId', createAlbum);
 router.post('/createMerchandise/:artistId', createMerchandise);
-
+router.get('/getMerchandises', getMerchandises);
 export { router };
