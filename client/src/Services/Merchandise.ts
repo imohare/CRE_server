ad0f996af2c2dfbd4124c7ed9133840fee42e8ce
aoniatraxler <traxler.aonia@gmail.com>
2022-03-22 18:25:22 +0000

    corrected merch api service

--- client/src/Services/Merchandise.ts
+++ client/src/Services/Merchandise.ts
@@ -1,14 +1,47 @@
-//createMerch
-const createMerch = () => { }
+import { IMerch } from "../Data/DataTypes/MerchTypes"
 
-//getAllMerch
-const getAllMerch = () => { }
+const BASE_URL = 'http://localhost:3001';
 
-//getMerchById
-const getMerchById = () => { }
+const createMerch = (merch: IMerch) => {
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(merch)
+    }
+    return fetch(BASE_URL, requestOptions)
+        .then(res => res.json())
+        .catch(err => console.log(err, "Error"))
+}
 
-//getMerchByArtist
-const getMerchByArtist = () => { }
+//getEvents
+const getMerch = () => {
+    return fetch(`${BASE_URL}/getMerch`)
+        .then(res => res.json())
+        .catch((err => console.log(err, "error")))
+}
 
-//getArtistByMerchandise (??)
-const getArtistByMerchandise = () => { }
+//getEventById
+const getMerchById = (merchId: number) => {
+    return fetch(`${BASE_URL}/getMerch/${merchId}`)
+        .then(res => res.json())
+        .catch((err => console.log(err, "error")))
+}
+
+
+//getEventsByArtist
+const getMerchByArtistId = (artistId: number) => {
+    return fetch(`${BASE_URL}/getArtistEvents/${artistId}`)
+        .then(res => res.json())
+        .catch((err => console.log(err, "error")))
+}
+
+//getArtistEvent --> personally do not see the point of this function ...
+const getArtistMerchByMerchAndArtistId = (merchId: number, artistId: number) => {
+    return fetch(`${BASE_URL}/getArtistEvent/${merchId}/${artistId}`)
+        .then(res => res.json())
+        .catch((err => console.log(err, "error")))
+}
+
+
+
+export { createMerch, getMerch, getMerchById, getMerchByArtistId, getArtistMerchByMerchAndArtistId }
