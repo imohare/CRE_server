dcb197019da733d2cd8ed39e14c4b795001863d5
aoniatraxler <traxler.aonia@gmail.com>
2022-03-22 16:29:31 +0000

    added album tokens

--- client/src/Services/AlbumToken.ts
+++ client/src/Services/AlbumToken.ts
@@ -1,27 +1,45 @@
-const BASE_URL = 'http://localhost:3001'
 import { IAlbumToken } from "Data/DataTypes"
+import { resolveSoa } from "dns"
+const BASE_URL = 'http://localhost:3001'
 
 //postAlbumToken
-const postAlbumToken = (album: IAlbumToken) => {
-    const requestOption = {
+const postAlbumToken = (album: IAlbumToken, artistId: number, consumerId: number) => {
+    const requestOptions = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify
-
+        body: JSON.stringify(album)
     }
-
+    return fetch(`${BASE_URL}/${artistId}/${consumerId}`, requestOptions)
+        .then(res => res.json())
+        .catch(err => console.log(err, "errors"))
 }
 
-
-
 //getAlbumTokenById
-const getAlbumTokenById = () => { }
+const getAlbumTokenById = (tokenId: number) => {
+    return fetch(`${BASE_URL}/getAlbumToken/${tokenId}`)
+        .then(res => res.json())
+        .catch(err => console.log(err, "errors"))
+}
 
 //getAlbumTokens
-const getAlbumTokens = () => { }
+const getAlbumTokens = () => {
+    return fetch(`${BASE_URL}/getAlbumTokens`)
+        .then(res => res.json())
+        .catch(err => console.log(err, "errors"))
+}
 
 //getConsumersAlbumTokens
-const getConsumersAlbumTokens = () => { }
+const getConsumersAlbumTokens = (albumId: number, consumerId: number) => {
+    return fetch(`${BASE_URL}/getConsumerAlbumTokens/${albumId}/${consumerId}`)
+        .then(res => res.json())
+        .catch(err => console.log(err, "errors"))
+}
 
 //getArtistsAlbumTokens
-const getArtistsAlbumTokens = () => { }
+const getArtistsAlbumTokens = (artistId: number) => {
+    return fetch(`${BASE_URL}/getArtistAlbumsTokens/${artistId}}`)
+        .then(res => res.json())
+        .catch(err => console.log(err, "errors"))
+}
+
+export { postAlbumToken, getAlbumTokenById, getAlbumTokens, getConsumersAlbumTokens, getArtistsAlbumTokens }
