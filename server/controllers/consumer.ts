f0d334e0082755ecf8d1f0df24bf5a073d24fae3
Imogen Hare <imogenbhare@gmail.com>
2022-03-23 11:10:13 +0000

    updated consumer controller to add profile picture and created getConsumerByUsername

--- server/controllers/consumer.ts
+++ server/controllers/consumer.ts
@@ -5,15 +5,28 @@ async function createConsumer(req: Request, res: Response) {
   const _consumer = await Consumer.create({
     eth_address: req.body.eth_address,
     username: req.body.username,
+    profile_picture: req.body.profile_picture,
     location: req.body.location,
     email: req.body.email,
   })
   res.send(_consumer);
 }
 
-async function getConsumer(req: Request, res: Response) {
+async function getConsumerById(req: Request, res: Response) {
   try {
     const _consumer = await Consumer.findByPk(req.params.consumerId);
+    res.json(_consumer);
+    res.status(200);
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+    res.json(error);
+  }
+}
+
+async function getConsumerByUsername(req: Request, res: Response) {
+  try {
+    const _consumer = await Consumer.findOne({where: {username: req.body.username}});
     res.json(_consumer);
     res.status(200);
   } catch (error) {
@@ -34,4 +47,4 @@ async function deleteConsumer(req: Request, res: Response) {
   res.json();
 }
 
-export { createConsumer, getConsumer, deleteConsumer }
+export { createConsumer, getConsumerById, getConsumerByUsername, deleteConsumer }
