a61d933fed0b70949244dd1620a3ea8cb9a92f7e
Imogen Hare <imogenbhare@gmail.com>
2022-03-24 15:01:40 +0000

    starting artist page

--- client/src/Pages/Artist.tsx
+++ client/src/Pages/Artist.tsx
@@ -1,7 +1,9 @@
-//react
 import { Link } from 'react-router-dom';
-//components
-//styling
+import { useState, useContext, useEffect } from 'react';
+import { UserContext } from 'Data/UserContext';
+
+
+
 
 const ArtistPage: React.FunctionComponent = () => {
 //if artist, display with additional update authorisation
@@ -10,6 +12,9 @@ const ArtistPage: React.FunctionComponent = () => {
     <>
     <Link to="/">home</Link>
     <h1>Artist</h1>
+    <div>
+      { useContext === artist ?<p>is artist</p> : <p>is not artist</p> }
+    </div>
     <p>artist view with option to update offers</p>
     <p>user view without any authorisations but with fake follow button</p>
     <Link to="/event">specific event</Link>
