3368c6f4e53c22ab96b197bc5d165e2139193fc6
Imogen Hare <imogenbhare@gmail.com>
2022-03-29 14:22:50 +0100

    background

--- client/src/Styles/globalStyles.ts
+++ client/src/Styles/globalStyles.ts
@@ -26,6 +26,11 @@ body {
   color: ${({ theme }) => theme.fontColor};
   highlight: ${({theme}) => theme.highlight}
   transition: all 0.5s linear;
+  background-image: url("https://firebasestorage.googleapis.com/v0/b/cre-6cbea.appspot.com/o/iStock-1199440598.jpg?alt=media&token=b5e7557e-414f-4293-93c5-5f8ce88ba61e");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: fixed;
+  background-attachement: fixed;
 }
 h1, h2, h3, h4, h5, h6 {
   color: #a7c2bf;
